51507a9628d451e4993be577158caabb86f8dc90
airizarry1025 <49927724+airizarry1025@users.noreply.github.com>
2019-07-17 14:45:15 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,71 @@
+ 
+//Display quiz
+var game = $("#trivia-questions");
+var questions = [
+    {
+        question:"What year did the Yankees Win the World Series?",
+        answer: ["a. 1999"],
+        answer: ["b. 2001"],
+        answer: ["c. 2010"],
+        answer: ["d. 1981"],
+        correctAnswer: "a. 1999"
+
+       
+        
+      },
+     
+    
+    {
+        question:"What year was I born?",
+        answer:["a. 1970"],
+        answer:["b. 1960"],
+        answer:["c. 1977"],
+        answer:["d. 1980"],
+        correctAnswer: "c. 1977"
+    },
+    {
+        question: "What City is the greatest city in the world?",
+        answer:["a .Chicago"],
+        answer:["b. New York"],
+        answer:["c. Miami"],
+        answer:["d. Hong Kong"],
+        correctAnswer: "d. Hong Kong"
+        
+
+    },
+];
+    console.log(questions);
+//Build the quiz
+function answerQuestion(){
+    answerQuestion(whatyeardidtheyankeeswintheworldseries)
+    answerQuestion(whatyeariwasborn)
+    answerQuestion(whatisthegreatestcityintheworld)
+
+
+}
+function buildQuiz(){}
+    
+
+function showResults(){}
+
+// display quiz right away
+buildQuiz();
+//for each question
+
+    
+   
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
